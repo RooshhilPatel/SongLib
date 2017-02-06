3c4e1acd0eeab4436b71ef6a096d045841e71723
RooshhilPatel <rushil.patel14@gmail.com>
2017-02-06 16:04:37 -0500

    first commit

---
+++ src/application/Song.java
@@ -0,0 +1,50 @@
+package application;
+
+public class Song {
+	
+	int year = 0;
+	String name = "";
+	String artist = "";
+	String album = "";
+	
+	public Song(){
+		this.name = "noName";
+		this.artist = "noArtist";
+	}
+	public Song(String name){
+		this.name = name;
+	}
+	public Song(String name, String artist){
+		this.name = name;
+		this.artist = artist;
+	}
+	public Song(String name, String artist, int year){
+		this.name = name;
+		this.artist = artist;
+		this.year = year;
+	}
+	public Song(String name, String artist, String album){
+		this.name = name;
+		this.artist = artist;
+		this.album = album;
+	}
+	public Song(String name, String artist, String album, int year){
+		this.name = name;
+		this.artist = artist;
+		this.album = album;
+		this.year = year;
+	}
+	public String getSongName(){
+		return this.name;
+	}
+	public String getSongArtist(){
+		return this.artist;
+	}
+	public String getSongAlbum(){
+		return this.album;
+	}
+	public int getSongYear(){
+		return this.year;
+	}
+	
+}
